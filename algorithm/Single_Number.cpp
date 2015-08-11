fed204d441334d29077cae7def5aab3799fd5398
JackieTseng <jackietseng86@gmail.com>
2015-08-11 22:45:43 +0800

    boring

---
+++ algorithm/Single_Number.cpp
@@ -0,0 +1,21 @@
+/*********************************************************************************
+ * Leetcode : Single Number -- https://leetcode.com/problems/single-number/
+ * Coded in Jul 1st, 2014
+ *
+ * [Problem]
+ * Given an array of integers, every element appears twice except for one. Find
+ * that single one.
+ *
+ * Notes:
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ *
+ *********************************************************************************/
+
+int singleNumber(int A[], int n) {
+    int num = A[0];
+    for (int i = 1; i < n; ++i) {
+        num ^= A[i];
+    }
+    return num;
+}
