443fc23bec74776576054a15a643246801849f21
JackieTseng <jackietseng86@gmail.com>
2015-08-22 12:19:04 +0800

    fix bug atoi

--- algorithm/String_to_Integer_atoi.cpp
+++ algorithm/String_to_Integer_atoi.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
 int myAtoi(string str) {
-    bool flag = (str[0] <= '9' && str[0] >= '0');
+    int index = 0;
+    while (str[index] == ' ' || str[index] == '\t') {
+        ++index;
+    }
+    int flag = 0;
+    if (str[index] == '-' || str[index] == '+') {
+        flag = (str[index++] == '-')? -1 : 1;
+    }
     int result = 0, length = str.length();
-    if (!flag) {
-        if (str[1] > '9' || str[1] < '0') {
-            return result;
+    for (; index < length && str[index] >= '0' && str[index] <= '9'; ++index) {
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && str[index] - '0' > 7)) {
+            if (flag == 0 || flag == 1) {
+                return INT_MAX;
+            } else {
+                return INT_MIN;
+            }
         }
+        result = result * 10 + (str[index] - '0' );
     }
-    for (int i = ((flag)? 0 : 1); i < length; i++) {
-        result *= 10;
-        result += (str[i] - '0' );
-    }
-    return ((flag)? 1 : ((str[0] == '-')? -1 : 1)) * result;
+    return ((flag == 0)? 1 : flag) * result;
 }
 
 int main(int argc, const char *argv[]) {
