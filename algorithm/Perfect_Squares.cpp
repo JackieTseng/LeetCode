b5e6400d31ccc9ad67a0d22865e7c82145a5e7e9
JackieTseng <jackietseng86@gmail.com>
2015-09-10 22:13:27 +0800

    1. static method 2. 4 way insert

--- algorithm/Perfect_Squares.cpp
+++ algorithm/Perfect_Squares.cpp
@@ -13,9 +13,11 @@
 #include <limits.h>
 #include <vector>
 #include <cmath>
+#include <set>
 using namespace std;
 
 // Solution One
+/*
 int numSquares(int n) {
     vector<int> dp(n + 1, INT_MAX);
     dp[0] = 0;
@@ -25,6 +27,53 @@ int numSquares(int n) {
         }
     }
     return dp[n];
+}
+*/
+
+// Solution Two
+/*
+int numSquares(int n) {
+    static vector<int> dp{0};
+    int m = dp.size();
+    if (m > n) {
+        return dp[n];
+    }
+    dp.resize(max(m, n + 1), INT_MAX);
+    for (int i = 0; i <= n; i++) {
+        for (int j = 1; i + j * j <= n; j++) {
+            dp[i + j * j] = min(dp[i + j * j], dp[i] + 1);
+        }
+    }
+    return dp[n];
+}
+*/
+
+// Solution Three
+int numSquares(int n) {
+    set<int> x1, x2;
+    int i = 1;
+    while (i * i <= n) {
+        x1.insert(i * i);
+        i++;
+    }
+    if (x1.find(n) != x1.end()) {
+        return 1;
+    }
+    set<int>::iterator p1, p2;
+    for (p1 = x1.begin(); p1 != x1.end(); p1++) {
+        for (p2 = x1.begin(); p2 != x1.end(); p2++) {
+            x2.insert((*p1) + (*p2));
+        }
+    }
+    if (x2.find(n) != x2.end()) {
+        return 2;
+    }
+    for (p1 = x1.begin(); p1 != x1.end(); p1++) {
+        if (x2.find(n - (*p1)) != x2.end()) {
+            return 3;
+        }
+    }
+    return 4;
 }
 
 int main(int argc, const char *argv[]) {
