ad67858308e2dc41ccffb34cc6db53a463cb9bfe
JackieTseng <jackietseng86@gmail.com>
2015-09-11 22:54:51 +0800

    new method for invert binary tree

--- algorithm/Invert_Binary_tree.cpp
+++ algorithm/Invert_Binary_tree.cpp
@@ -1,4 +1,24 @@
+/*********************************************************************************
+ * Leetcode : Invert Binary Tree -- https://leetcode.com/problems/invert-binary-tree/
+ * Coded in Seq 11th, 2015
+ *
+ * [Problem]
+ * Invert a binary tree.
+ * 
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * to
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *********************************************************************************/
 #include <iostream>
+#include <queue>
 using namespace std;
 
 struct TreeNode {
@@ -8,6 +28,7 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+// Solution One
 TreeNode* invertTree(TreeNode* root) {
     if (root == NULL) {
         return root;
@@ -18,7 +39,30 @@ TreeNode* invertTree(TreeNode* root) {
     return root; 
 }
 
+// Solution Two
+TreeNode* invertTree(TreeNode* root) {
+    if (root == NULL) {
+        return root;
+    }
+    queue<TreeNode*> s;
+    s.push(root);
+    while (!s.empty()) {
+        TreeNode* cur = s.front();
+        s.pop();
+        TreeNode* t = cur->left;
+        cur->left = cur->right;
+        cur->right = t;
+
+        if (cur->left != NULL) {
+            s.push(cur->left);
+        }
+        if (cur->right != NULL) {
+            s.push(cur->right);
+        }
+    }
+    return root;
+}
+
 int main(int argc, const char *argv[]) {
-    
     return 0;
 }
